b7055293e57a2add27157a52aea8c279132b8462
benardopiyo <benardopiyo13@gmail.com>
2025-06-05 15:52:52 +0300

    feat: Add sign-in form UI with email/username and password fields
    - Includes login form with basic validation and submit button
    - Provides option to navigate to account creation via sign-up button

--- frontend/static/js/pageContext/login.js
+++ frontend/static/js/pageContext/login.js
@@ -1,11 +1,11 @@
 export const signinContext = `
     <div class="Form-div">
         <form class="loggin siggnup" id="loginForm">
-            <label>Login</label><br>
-            <input type="text" class="input" placeholder="email/username" name="email" required><br>
-            <input type="password" class="input" placeholder="password" name="password" required><br>
-            <input type="submit" value="Login" id="form-loginBtn">
+            <label>Welcome Back</label>
+            <input type="text" class="input" placeholder="Email or Username" name="email" required>
+            <input type="password" class="input" placeholder="Password" name="password" required>
+            <input type="submit" value="Sign In" id="form-loginBtn">
         </form>
-        <p >Dont Have an account? <button id="signUpBtn">signup</button> </p> 
+        <p>Don't have an account? <button id="signUpBtn">Create one</button></p> 
     </div>
-` 
+`;
