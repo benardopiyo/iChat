60cfc9b10c001e2293e32f25bb73747a1e6a320a
benardopiyo <benardopiyo13@gmail.com>
2025-06-05 15:54:55 +0300

    feat: Add user sign-up form with full profile input fields
    - Includes fields for username, name, gender, age, email, and password
    - Implements password confirmation and basic validation
    - Provides navigation option for existing users to sign in

--- frontend/static/js/pageContext/signup.js
+++ frontend/static/js/pageContext/signup.js
@@ -1,40 +1,22 @@
 export const signupContext = `
-<style>
-    @media (min-width: 480px) and (max-width: 980px) {
-    .Form-div {
-        padding: 1.5rem;
-    }
-    .signup-Form-div{
-        padding: 1.5rem;
-    }
-    .Form-div {
-        height: 100%;
-    }
-
-    label {
-        font-size: 1.2rem;
-    }
-}
-
-</style>
     <div class="Form-div">
-        <form class="loggin siggnup" id="signUpForm" >
-            <label>Signup</label><br>
-            <input type="text" name="username" class="input" placeholder="Username" required><br>
-            <input type="text" name="firstName" class="input" placeholder="First Name" required><br>
-            <input type="text" name="lastName" class="input" placeholder="Last Name" required><br>
-                <select name="gender" class="input" required>
-                    <option value="" disabled selected>Gender (Male/Female/Other)</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Other">Other</option>
-                </select><br>
-            <input type="number" name="age" class="input" placeholder="Age" min=1 required><br>
-            <input type="email" name="email" class="input" placeholder="email-address" required><br>
-            <input type="password" name="password" class="input" placeholder="password" required><br>
-            <input type="password" name="password2" class="input" placeholder="Confirm password" required><br>
-            <input type="submit" value="Sign-up">
+        <form class="loggin siggnup" id="signUpForm">
+            <label>Join RealForum</label>
+            <input type="text" name="username" class="input" placeholder="Username" required>
+            <input type="text" name="firstName" class="input" placeholder="First Name" required>
+            <input type="text" name="lastName" class="input" placeholder="Last Name" required>
+            <select name="gender" class="input" required>
+                <option value="" disabled selected>Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+            </select>
+            <input type="number" name="age" class="input" placeholder="Age" min="13" max="120" required>
+            <input type="email" name="email" class="input" placeholder="Email Address" required>
+            <input type="password" name="password" class="input" placeholder="Password (min. 8 characters)" required>
+            <input type="password" name="password2" class="input" placeholder="Confirm Password" required>
+            <input type="submit" value="Create Account">
         </form>
-      <p>Have an account? <button id="loginBtn">login</button> </p> 
+        <p>Already have an account? <button id="loginBtn">Sign in</button></p> 
     </div>
-`
+`;
